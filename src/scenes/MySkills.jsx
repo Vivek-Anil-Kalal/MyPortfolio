1460a877df90554b3f6fa3756e8a515bd225e30d
Vivek Kalal <vivekkalal.nextech@gmail.com>
2023-04-07 16:57:49 +0530

    Innovative section is added in Skills section

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -89,6 +89,32 @@ const MySkills = () => {
                     whileInView="visible"
                     // this shows thas 0.5(50%) jab ye div viewport par ayga tab once ye animation chalega
                     viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 }
+                    }}
+                >
+
+                    <div className="relative h-32">
+                        <div className="z-10">
+                            <p className="font-playfair font-semibold text-5xl">02</p>
+                            <p className="font-playfair font-semibold text-3xl mt-3">Innovative</p>
+                        </div>
+                        <div className="w-1/2 md:w/3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+                    </div>
+                    <p className="mt-5">
+                        As an innovative individual, I am constantly seeking out new and creative ways to approach challenges and achieve success. One example of my innovative thinking is demonstrated through my creation of a TechBlog Website that boasts strong functionalities and an innovative UI design. In addition to my technical skills, I am also constantly generating new business ideas that I believe can be successfully implemented. I am confident in my ability to not only generate these ideas, but to also see them through to fruition, utilizing my skills in web development and beyond to bring these concepts to life. Overall, I believe that my combination of technical and business acumen makes me a valuable asset to any team or project, and I am excited to continue pursuing my passions and pushing the boundaries of what is possible.
+                    </p>
+                </motion.div>
+
+                {/* IMAGINATIVE */}
+                <motion.div
+                    className="md:w-1/3 mt-10"
+                    initial="hidden"
+                    whileInView="visible"
+                    // this shows thas 0.5(50%) jab ye div viewport par ayga tab once ye animation chalega
+                    viewport={{ once: true, amount: 0.5 }}
                     transition={{ duration: 0.5 }}
                     variants={{
                         hidden: { opacity: 0, y: 50 },
